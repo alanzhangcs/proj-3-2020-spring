51943a8c1f4b742c8a574d45a33abe28273913a3
Wcx <1669807991@qq.com>
2020-04-15 22:23:20 +0800

    完成了全部基础流程。数据集来源：mnist-fashion
    Signed-off-by: Wcx <1669807991@qq.com>

--- README.md
+++ README.md
@@ -3,4 +3,12 @@ para_dot_noalgo为未改算法版本。
 para_dot修改了算法（计算顺序），但不得不提前赋值。希望能继续改进。
 
 naive.cpp:一个未优化的串行实现版本。
-## 
+## naive.cpp 函数说明
+Matrix load_data(const char* filename): 从文件中读取矩阵（快速版）
+void write_data(const char* filename, Matrix m): 将一个矩阵写到文件里
+Matrix mat_mul(Matrix a, Matrix b): 返回一个新矩阵，原矩阵不动
+void normalize(Matrix res): 矩阵按行线性归一化。
+void softmax(Matrix res): 直接在当前矩阵上操作，将矩阵按行softmax
+double cross_entropy_loss(Matrix logits, Matrix ground_truth): 对输出值（必须全为正数，默认已经softmax）和label做交叉熵
+Matrix logits2prediction(Matrix logits): 将softmax后的输出值转化为0-1的预测值（生成新的矩阵）。
+double accuracy(Matrix prediction, Matrix ground_truth) : 对预测值和labels计算准确率.
--- naive.cpp
+++ naive.cpp
@@ -17,23 +17,50 @@ Matrix load_data(const char* filename){
     char* buf = (char*)malloc(100);
     fgets(buf, 100, f);
     sscanf(buf, "%d %d", &(m.row), &(m.col));
+    free(buf);
 
-    int row_max_len = m.col*7 > 200 ? m.col * 7:200;
-    buf = (char *)malloc(row_max_len);
+    int max_len = m.row * m.col * 7;
+    buf = (char *)malloc(max_len);
     int cursor = 0;
     m.data = (double *)malloc(sizeof(double) * m.row * m.col);
     if(m.data == NULL){
         printf("Out of memory error!\n");
         exit(1);
     }
-    int line_cursor;
+    fread(buf, 1, max_len, f);
+    // printf("%c%c%c", buf[0], buf[1], buf[2]);
+
+    int char_cursor = 0;
+    double temp_num;
+    char ch;
+    double float_count;
     for(int i = 0; i < m.row; i++){
-        fgets(buf, row_max_len, f);
-        line_cursor = 0;
+        // if(i % 1000 == 0)
+        //     printf("loading data: %% %f\n", (double)i / m.row);
         for(int j = 0; j < m.col; j++){
-            m.data[cursor++] = atof(buf + line_cursor);
-            while(buf[line_cursor] != ' ' && buf[line_cursor] != '\n')
-                line_cursor++;
+            temp_num = 0;
+            float_count = 10;
+            ch = buf[char_cursor++];
+
+            bool is_neg = ch == '-'?1:0;
+            if (ch == '-')       
+                ch = buf[char_cursor++];
+
+            while(ch != ' ' && ch != '\n'){
+                if(ch=='.'){
+                    ch = buf[char_cursor++];
+                    break;
+                }
+                temp_num *= 10;
+                temp_num += ch - 48;
+                ch = buf[char_cursor++];
+            }
+            while(ch != ' ' && ch != '\n'){
+                temp_num += (ch - 48) / float_count;
+                float_count *= 10;
+                ch = buf[char_cursor++];
+            }
+            m.data[cursor++] = is_neg ? -temp_num:temp_num;
         }
     }
 
@@ -75,35 +102,27 @@ Matrix mat_mul(Matrix a, Matrix b) {
     for (int m=0; m < res.row; m++) {
         for (int s = 0; s < res.col; s++) {
             res.data[m * res.col + s] = 0;
-            for (int n = 0; n < res.col; n++) {
-                res.data[m * res.col + s] += a.data[m * a.col + n] * b.data[n * b.row + s];
+            for (int n = 0; n < a.col; n++) {
+                res.data[m * res.col + s] += a.data[m * a.col + n] * b.data[n * b.col + s];
             }
+            // if( res.data[m * res.col + s] == 0)
+            //     printf("%d %d %d\n",m ,s);
         }
     }
     return res;
 }
 
 
-Matrix mat_add_vec(Matrix a, Matrix b, int axis) {
-    Matrix res;
-    if (axis == 0 ? a.row : a.col != b.row) {
-        printf("Error! matrix dim not match.\n");
-        printf("Calc %d,%d + %d,%d \n", a.row, a.col, b.row, b.col);
-        return res;
-    }
-
-    res.row = a.row;
-    res.col = a.col;
-    res.data = (double *)malloc(sizeof(double) * res.row * res.col); 
+void normalize(Matrix res) {
+    double sum;
     for (int m=0; m < res.row; m++) {
-        for (int s = 0; s < res.col; s++) {
-            if(axis == 0)
-                res.data[m * res.col + s] = a.data[m * res.col + s] + b.data[m];
-            else
-                res.data[m * res.col + s] = a.data[m * res.col + s] + b.data[s];
+        sum = 0;
+        for (int s = 0; s < res.col; s++){
+            sum += res.data[m * res.col + s];
         }
+        for (int s = 0; s < res.col; s++) 
+            res.data[m * res.col + s] = res.data[m * res.col + s] / sum;
     }
-    return res;
 }
 
 
@@ -121,26 +140,107 @@ void softmax(Matrix res) {
 }
 
 
+double cross_entropy_loss(Matrix logits, Matrix ground_truth) {
+    // Suppose the logits have already softmaxed
+    if (logits.row != ground_truth.row || logits.col != ground_truth.col) {
+        printf("Error! matrix dim not match.\n");
+        printf("Calc %d,%d * %d,%d \n", logits.row, logits.col, ground_truth.row, ground_truth.col);
+        return -1;
+    }
+    double sum = 0;
+    double clip;
+    // printf("%f", log(0.001));
+    for (int m=0; m < logits.row; m++) {
+        for (int s = 0; s < logits.col; s++){
+            if(ground_truth.data[m * logits.col + s] != 0){
+                clip = 1 - logits.data[m * logits.col + s];
+                clip = clip < 0.001 ? 0.001: clip;
+                // if(isnan(log(clip)))
+                //     printf("%f %f", clip, logits.data[m * logits.col + s]);
+                sum -= ground_truth.data[m * logits.col + s] * log(clip);
+                // if(isnan(sum))
+                //     printf("%f", clip);
+            }
+        }
+    }
+    return sum / logits.row;
+}
+
+
+Matrix logits2prediction(Matrix logits) {
+    // Suppose the logits have already softmaxed
+    Matrix res;
+    res.row = logits.row;
+    res.col = logits.col;
+    res.data = (double*)malloc(sizeof(double) * res.row * res.col);
+    int arg_max;
+    double row_max;
+    for (int m=0; m < logits.row; m++) {
+        row_max = -1;
+        arg_max = 0;
+        for (int s = 0; s < logits.col; s++){
+            res.data[m * res.col + s] = 0;
+            if (logits.data[m * res.col + s] > row_max){
+                // printf("%f %f\n", row_max, logits.data[m * res.col + s]);
+                row_max = logits.data[m * res.col + s];
+                arg_max = s;
+            }
+        }
+        res.data[m * res.col + arg_max] = 1;
+        // printf("%d ", arg_max);
+    }
+    return res;
+}
+
+
+double accuracy(Matrix prediction, Matrix ground_truth) {
+    if (prediction.row != ground_truth.row || prediction.col != ground_truth.col) {
+        printf("Error! matrix dim not match.\n");
+        printf("Calc %d,%d * %d,%d \n", prediction.row, prediction.col, ground_truth.row, ground_truth.col);
+        return -1;
+    }
+    int row = prediction.row, col = prediction.col;
+    double acc = 0;
+    int total_num = ground_truth.row;
+    double temp_sum;
+    bool correct;
+    for (int m=0; m < row; m++) {
+        temp_sum = 0;
+        correct = true;
+        for (int s = 0; s < col; s++){
+            correct = correct? prediction.data[m * col + s] == ground_truth.data[m * col + s]: correct;
+            temp_sum += ground_truth.data[m * col + s];
+        }
+        acc += correct && temp_sum != 0 ? 1 : 0;
+        total_num -= temp_sum == 0 ? 1 : 0;
+    }
+    // printf("%f %d\n", acc, total_num);
+    return acc / total_num;
+}
+
+
 int main() {
     clock_t t;
     t = clock();
-    Matrix bias = load_data("data/bias.txt");
     Matrix features = load_data("data/features.txt");
     Matrix labels = load_data("data/labels.txt");
     Matrix w = load_data("data/w.txt");
+    normalize(features);
     printf("Load data: %fs\n", (double)(clock() - t) / CLOCKS_PER_SEC);
 
     t = clock();
-    Matrix output1 = mat_mul(features, w);
-    Matrix output2 = mat_add_vec(output1, bias, 1);
-    // write_data("test1.txt", output2);
-    // free(output1.data);
-    softmax(output2);
+    Matrix output = mat_mul(features, w);
+    softmax(output);
+    // write_data("test2.txt", output);
+    double loss = cross_entropy_loss(output, labels);
+    Matrix prediction = logits2prediction(output);
+    double acc = accuracy(prediction, labels);
     printf("Calculate time: %fs\n", (double)(clock() - t) / CLOCKS_PER_SEC);
-
-    write_data("test.txt", output2);
+    
+    printf("Result: loss = %f, acc = %f", loss, acc);
+    write_data("prediction.txt", prediction);
     // Matrix features = load_data("data/features.txt");
-
+    return 0;
 }
 
 
